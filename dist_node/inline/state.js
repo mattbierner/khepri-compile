6da5a330c509e858fd28f3e17d595634b8704f35
Matt Bierner <mattbierner@gmail.com>
2014-04-28 11:36:38 -0400

    Removed bindings fole

--- dist_node/inline/state.js
+++ dist_node/inline/state.js
@@ -4,36 +4,36 @@
 */"use strict";
 var record = require("bes")["record"],
     hamt = require("hamt"),
-    binding = require("./bindings"),
     __o = require("./binding"),
     Binding = __o["Binding"],
     MUTABLE = __o["MUTABLE"],
     IMMUTABLE = __o["IMMUTABLE"],
     State, getBinding, push, pop, getGlobals, addGlobal;
 (State = record.declare(null, ["bindings", "working", "globals", "outer"]));
-(State.empty = new(State)(binding.empty, binding.empty, hamt.empty, null));
+(State.empty = new(State)(hamt.empty, hamt.empty, hamt.empty, null));
 (State.prototype.addBinding = (function(uid, target, simple) {
     var s = this;
-    return s.setBindings(binding.setBinding(uid, Binding.create(target, IMMUTABLE, simple), s.bindings));
+    return s.setBindings(hamt.set(uid, Binding.create(target, IMMUTABLE, simple), s.bindings));
 }));
 (State.prototype.addWorking = (function(uid, target, simple) {
     var s = this;
-    return s.setWorking(binding.setBinding(uid, Binding.create(target, MUTABLE, simple), s.working));
+    return s.setWorking(hamt.set(uid, Binding.create(target, MUTABLE, simple), s.working));
 }));
 (push = (function(s) {
     return s.setOuter(s)
-        .setWorking(binding.empty);
+        .setWorking(hamt.empty);
+}));
+var nullWorking = hamt.fold.bind(null, (function(p, __o0) {
+    var key = __o0["key"];
+    return hamt.set(key, null, p);
 }));
 (pop = (function(s) {
     return s.outer.setBindings(s.bindings)
         .setGlobals(s.globals)
-        .setWorking(hamt.fold((function(p, __o0) {
-            var key = __o0["key"];
-            return hamt.set(key, null, p);
-        }), s.outer.working, s.working));
+        .setWorking(nullWorking(s.outer.working, s.working));
 }));
 (getBinding = (function(uid, s) {
-    return (binding.getBinding(uid, s.bindings) || binding.getBinding(uid, s.working));
+    return (hamt.get(uid, s.bindings) || hamt.get(uid, s.working));
 }));
 var y = hamt.keys;
 (getGlobals = (function(z) {
